f98c1093005faf4385c83488a77327878a065f70
André <comin.andre842@gmail.com>
2025-04-15 14:57:29 -0300

    segunda alteração no arquivo

--- pratica.js
+++ pratica.js
@@ -1,1 +1,21 @@
-console.log("Olá, mundão")
+let nomeHeroi = "Ginka"
+let quantidadeXp = 1000
+let nivel
+
+if (quantidadeXp <= 1000) {
+    nivel = "Ferro"
+} else if (quantidadeXp >= 1001 && quantidadeXp <= 2000) {
+    nivel = Bronze
+} else if (quantidadeXp >= 2001 && quantidadeXp <= 5000) {
+    nivel = "Prata"
+} else if (quantidadeXp >= 5001 && quantidadeXp <= 7000) {
+    nivel = "Ouro"
+} else if (quantidadeXp >= 7001 && quantidadeXp <= 9000) {
+    nivel = "Ascendente"
+} else if (quantidadeXp >= 9001 && quantidadeXp <= 10000) {
+    nivel = "Imortal"
+} else if (quantidadeXp >= 10001) {
+    nivel = "Radiante"
+}
+
+console.log(`O jogador ${nomeHeroi} está no nível de ${nivel}.`)
